fcb804d864a745df7c9564cc655cdbf0552dd945
RojeCodes <idiaroje@gmail.com>
2024-08-08 11:39:08 +0800

    Imported Chart.js and made a pie chart out of hardcoded data to test if it works

--- client/src/pages/logsStatistics.jsx
+++ client/src/pages/logsStatistics.jsx
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
+import Chart from 'chart.js/auto';
 import NavBarReyes from '../components/navBar';
 import NavBarGroup1 from './Navbar.jsx';
 
@@ -23,6 +24,40 @@ const LogStatistics = () => {
         fetchLogs();
     }, []);
 
+    const chartRef = useRef(null);
+    const chartInstance = useRef(null);
+
+    useEffect(() => {
+        if (chartInstance.current) {
+            chartInstance.current.destroy()
+        }
+        const myChartRef = chartRef.current.getContext('2d');
+
+        chartInstance.current=new Chart(myChartRef, {
+            type: "pie",
+            data: {
+                labels: ["Label 1", "Label 2", "Label 3"],
+                datasets: [
+                    {
+                        data: [300, 50, 100],
+                        backgroundColor: [
+                            'rgb(255, 99, 132)',
+                            'rgb(54, 162, 235)',
+                            'rgb(255, 205, 86)'
+                        ],
+                    }
+                ]
+            }
+        });
+
+        return () => {
+            if (chartInstance.current) {
+                chartInstance.current.destroy();
+            }
+        }
+    }, []);
+
+    
     return (
         <>
             <NavBarGroup1 />
@@ -36,6 +71,19 @@ const LogStatistics = () => {
                 <p className="text-lg text-gray-300">
                     Stats go here, probably gonna add a graph of some sort
                 </p>
+
+                <div class="chart-container">
+                    <canvas ref={chartRef} style={{width:"300px", height:"200px"}}/>
+                </div>
+
+                <div class="details">
+                    <ul>
+                        <li>GET: <span class="percentage">40%</span></li>
+                        <li>POST: <span class="percentage">20%</span></li>
+                        <li>PUT: <span class="percentage">30%</span></li>
+                        <li>DELETE: <span class="percentage">10%</span></li>
+                    </ul>
+                </div>
             </div>
         </>   
     );
