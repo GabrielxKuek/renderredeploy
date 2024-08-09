664a702c452724fae05b94ba21ef29b2c7da96f1
GabrielxKuek <kuekguanjiegabriel@gmail.com>
2024-08-09 20:44:19 +0800

    test render

--- server/src/models/modificationModel.js
+++ server/src/models/modificationModel.js
@@ -18,7 +18,7 @@ export const insertModification = async (user_id, site_id, table_name, record_id
 
 export const selectModificationByAll = async (site_id) => {
   try {
-    const result = await prisma.$queryRaw`SELECT * FROM viewModification(${site_id}::INT);`
+    const result = await prisma.$queryRaw`SELECT * FROM viewModification(1);`
     return result;
   } catch (error) {
     console.error('Error reading modification logs:', error);
