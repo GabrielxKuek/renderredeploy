747c7cd143966247ff5b41823d8d114d9d9e5760
xR4F4ELx <rafael_macam@yahoo.com>
2024-08-10 15:54:09 +0800

    Updated log request

--- server/src/middleware/logRequestMiddleware.js
+++ server/src/middleware/logRequestMiddleware.js
@@ -3,26 +3,40 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 const logRequestMiddleware = async (req, res, next) => {
-  // Function to log request details
-  try {
-    const { request_method, api_requested, user_ip, user_os } = req.logData;
-    await prisma.um_request_log.create({
-      data: {
-        request_method: request_method || 'UNKNOWN_METHOD',
-        api_requested: api_requested || 'UNKNOWN_API',
-        user_ip: user_ip || 'UNKNOWN_IP',
-        user_os: user_os || 'UNKNOWN_OS',
-        created_at: new Date(),
-        error_message: 'NO_ERROR',
-        site_id: req.site_id !== undefined ? parseInt(req.site_id, 10) : null,
-        user_id: req.user_id !== undefined ? parseInt(req.user_id, 10) : null
-      }
-    });
-    next();
-    console.log('Request logged to the database successfully');
-  } catch (dbError) {
-    console.error('Failed to log request to database:', dbError);
-  }
-}
+    try {
+        // Safely access the properties from req.logData
+        const {
+            request_method = 'UNKNOWN_METHOD',
+            api_requested = 'UNKNOWN_URL',
+            user_ip = 'UNKNOWN_IP',
+            user_os = 'UNKNOWN_USER_OS',
+            headers = {},
+            body = {}
+        } = req.logData || {};
 
-export {logRequestMiddleware} ;
+        const { user_id, site_id } = req;
+
+        await prisma.um_request_log.create({
+            data: {
+                request_method,
+                api_requested,
+                user_ip,
+                user_os,
+                created_at: new Date(),
+                error_message: 'NO_ERROR', // Include any error message if present
+                site_id: site_id !== undefined ? parseInt(site_id, 10) : null,
+                user_id: user_id !== undefined ? parseInt(user_id, 10) : null,
+                headers: JSON.stringify(headers),  // Store headers as a separate column
+                body: JSON.stringify(body)         // Store body as a separate column
+            }
+        });
+
+        console.log('Request logged to the database successfully');
+    } catch (dbError) {
+        console.error('Failed to log request to database:', dbError);
+    }
+
+    next();
+};
+
+export { logRequestMiddleware };
