79e5903102207d75e338456d97262625c682dc83
RojeCodes <idiaroje@gmail.com>
2024-08-19 18:38:26 +0800

    Added filters for user_id and table_name on logStats

--- client/src/pages/logsStatistics.jsx
+++ client/src/pages/logsStatistics.jsx
@@ -28,15 +28,17 @@ const LogStatistics = () => {
     const [requestLogs, setRequestLogs] = useState([]);
     const [loading, setLoading] = useState(false);
     const [isDropdownVisible, setIsDropdownVisible] = useState(false);
-    const [selectedFilter, setSelectedFilter] = useState('creation');
+    const [searchValue, setsearchValue] = useState(""); 
+    const [selectedFilter, setSelectedFilter] = useState('All');
+    const [selectedRadio, setSelectedRadio] = useState('user_id');
 
-    const fetchLogs = async (filter, setLogType) => {
+    const fetchLogs = async (filter) => {
         setLoading(true);
         try {
             //const response = useRender ? await axios.get(`https://authinc-inc2024-group6-s17i.onrender.com/api/${filter}/viewAll`) : await axios.get(`http://localhost:8081/api/${filter}/viewAll`);
-            const createLogs = useRender ? await axios.get(`${apiUrl}/api/creation/viewAll`) : await axios.get(`http://localhost:8081/api/creation/viewAll`);
-            const modifyLogs = useRender ? await axios.get(`${apiUrl}/api/modification/viewAll`) : await axios.get(`http://localhost:8081/api/modification/viewAll`);
-            const deleteLogs = useRender ? await axios.get(`${apiUrl}/api/deletion/viewAll`) : await axios.get(`http://localhost:8081/api/deletion/viewAll`);
+            const createLogs = useRender ? await axios.get(`${apiUrl}/api/creation/view${filter}`) : await axios.get(`http://localhost:8081/api/creation/view${filter}`);
+            const modifyLogs = useRender ? await axios.get(`${apiUrl}/api/modification/view${filter}`) : await axios.get(`http://localhost:8081/api/modification/view${filter}`);
+            const deleteLogs = useRender ? await axios.get(`${apiUrl}/api/deletion/view${filter}`) : await axios.get(`http://localhost:8081/api/deletion/view${filter}`);
             const reqLogs = useRender ? await axios.get(`${apiUrl}/api/request/viewAll`) : await axios.get(`http://localhost:8081/api/request/viewAll`);
             setLogs({
                 creationLogs: createLogs.data,
@@ -55,12 +57,12 @@ const LogStatistics = () => {
     };
 
     useEffect(() => {
-        fetchLogs(selectedFilter, setLogs);
+        fetchLogs(selectedFilter);
     }, []);
 
     useEffect(() => {
         const interval = setInterval(() => {
-            fetchLogs(selectedFilter, setLogs);
+            fetchLogs(selectedFilter);
         }, 1000); // Update logs every second
 
         return () => clearInterval(interval); // Clear interval on component unmount
@@ -70,12 +72,80 @@ const LogStatistics = () => {
         setIsDropdownVisible(!isDropdownVisible);
     }
 
-    const handleFilterSelect = (filter) => {
-        setSelectedFilter(filter);
-        setIsDropdownVisible(false);
-        navigate(`/logsStatistics`);
+    const handleRadioChange = (event) => {
+        setSelectedRadio(event.target.value);
     }
     
+    const handleSearchSubmit = async (event) => {
+        setLoading(true);
+        event.preventDefault();
+        try {
+            const validOptions = ['table_name', 'user_id'];
+            
+            if (!validOptions.includes(selectedRadio)) {
+                throw new Error(`Invalid selectedSearchOption: ${selectedRadio}. Must be one of ${validOptions.join(', ')}.`);
+            }
+
+            const creationLogs = useRender
+                ? await axios.get(`${apiUrl}/api/search/creation`, {
+                    headers: { Authorization: `Bearer ${jwt}` },
+                    params: { searchValue, selectedRadio }
+                })
+                : await axios.get(`http://localhost:8081/api/search/creation`, {
+                    headers: { Authorization: `Bearer ${jwt}` },
+                    params: { searchValue, selectedRadio }
+                });
+
+            const modifyLogs = useRender
+                ? await axios.get(`${apiUrl}/api/search/modification`, {
+                    headers: { Authorization: `Bearer ${jwt}` },
+                    params: { searchValue, selectedRadio }
+                })
+                : await axios.get(`http://localhost:8081/api/search/modification`, {
+                    headers: { Authorization: `Bearer ${jwt}` },
+                    params: { searchValue, selectedRadio }
+                });
+
+            const deletionLogs = useRender
+                ? await axios.get(`${apiUrl}/api/search/deletion`, {
+                    headers: { Authorization: `Bearer ${jwt}` },
+                    params: { searchValue, selectedRadio }
+                })
+                : await axios.get(`http://localhost:8081/api/search/deletion`, {
+                    headers: { Authorization: `Bearer ${jwt}` },
+                    params: { searchValue, selectedRadio }
+                });
+            /*
+            const reqLogs = useRender
+                ? await axios.get(`${apiUrl}/api/request/viewAllBy${selectedRadio}`, {
+                    headers: { Authorization: `Bearer ${jwt}` },
+                    params: { searchValue }
+                })
+                : await axios.get(`http://localhost:8081/api/request/viewAllBy${selectedRadio}`, {
+                    headers: { Authorization: `Bearer ${jwt}` },
+                    params: { searchValue }
+                }); */
+
+                if (creationLogs.data.length === 0 && modifyLogs.data.length === 0 && deletionLogs.data.length === 0 && reqLogs.data.length === 0) {
+                    setError('No logs found for \'' + searchValue + '\'');
+                } else {
+                    setLogs({
+                        creationLogs: creationLogs.data,
+                        modificationLogs: modifyLogs.data,
+                        deletionLogs: deletionLogs.data,
+                        requestLogs: logs.requestLogs
+                    })
+                    setSelectedFilter(selectedRadio)
+                    setError(null);
+                }
+        } catch (error) {
+            console.error('Error searching logs:', error);
+            setError('Error searching logs. Please try again.');
+        } finally {
+            setLoading(false);
+            setsearchValue("")
+        }
+    };
 
     return (
         <>
@@ -83,9 +153,88 @@ const LogStatistics = () => {
             <NavBarReyes />
 
             <div className="flex flex-col justify-center items-center h-screen bg-gray-800">
-                <h1 className="text-6xl font-extrabold text-white mb-4">
+                <h1 className="text-6xl font-extrabold text-white mt-4">
                     log<span className="text-yellow-500">Stats</span>
                 </h1>
+
+                {/*<p className="text-lg text-gray-300 mx-3.5">
+                    Statistics from Auth INC
+                </p>*/}
+
+                <div className="w-11/12 max-w-4xl mb-4 mt-4">
+                    
+                    <form onSubmit={handleSearchSubmit} className="flex flex-col items-start w-full">
+                        <div className="flex items-center mb-4 w-full">
+                            {/* Descriptive Text */}
+                            <p className="text-md text-gray-50 mb-0">
+                                Search for logs..
+                            </p>
+
+                            {/* Radio Buttons */}
+                            <div className="flex ml-auto">
+                                    <label className="mr-4 text-gray-50">
+                                        <input
+                                            type="radio"
+                                            value="user_id"
+                                            checked={selectedRadio === "user_id"}
+                                            onChange={handleRadioChange}
+                                            className="mr-2"
+                                        />
+                                        User ID
+                                    </label>
+
+                                    <label className="mr-4 text-gray-50">
+                                        <input
+                                            type="radio"
+                                            value="table_name"
+                                            checked={selectedRadio === 'table_name'}
+                                            onChange={handleRadioChange}
+                                            className="mr-2"
+                                        />
+                                        Table Name
+                                    </label>
+                                    {/*
+                                    <label className="text-gray-50 mr-4">
+                                        <input
+                                            type="radio"
+                                            value="os"
+                                            checked={selectedRadio === 'os'}
+                                            onChange={handleRadioChange}
+                                            className="mr-2"
+                                        />
+                                        OS
+                                    </label>
+                                    
+                                    <label className="text-gray-50">
+                                        <input
+                                            type="radio"
+                                            value="ip"
+                                            checked={selectedRadio === 'ip'}
+                                            onChange={handleRadioChange}
+                                        />
+                                        IP
+                                    </label>*/}
+                            </div>
+                        </div>
+                        {/* Search Box */}
+                        <div className="flex items-center w-full">
+                            <input
+                                type="text"
+                                placeholder="Search..."
+                                value={searchValue}
+                                onChange={/*handleSearchChange*/null}
+                                className="flex-grow px-4 py-2 border border-gray-300 rounded-l-md text-black"
+                            />
+                            <button
+                                type="submit"
+                                className="px-4 py-2 bg-indigo-500 text-white rounded-r-md hover:bg-indigo-600 active:bg-indigo-700"
+                            >
+                                Search
+                            </button>
+                        </div>
+                    </form>
+
+                </div>
                 
                 {/* fetch data */}
                 
@@ -115,7 +264,7 @@ const LogStatistics = () => {
                         <div className="flex justify-between items-center">
                             <button
                                 onClick={() => fetchLogs(selectedFilter)}
-                                className="px-4 py-2 bg-indigo-500 text-white rounded hover:bg-indigo-600 active:bg-indigo-700 flex items-center"
+                                className="px-4 py-2 me-2 bg-indigo-500 text-white rounded hover:bg-indigo-600 active:bg-indigo-700 flex items-center"
                                 disabled={loading}
                             >
                                 {loading ? (
@@ -126,7 +275,12 @@ const LogStatistics = () => {
                                 ) : null}
                                 Refreshing data
                             </button>
-
+                            <button
+                                onClick={() => setSelectedFilter("All")}
+                                className="px-4 py-2 bg-indigo-500 text-white rounded hover:bg-indigo-600 active:bg-indigo-700 flex items-center"
+                            >
+                                Remove Filters
+                            </button>
 
                         </div>
                     )}
@@ -201,7 +355,7 @@ const LogStatistics = () => {
                                     requestLogs: logs.requestLogs
                                 }}
                             />
-                        </div> {/*
+                        </div>
                         <div className="line-container col-8">
                             <LineChart 
                                 logs={{
@@ -211,7 +365,6 @@ const LogStatistics = () => {
                                 }}
                             />
                         </div>
-*/}
                     </div>
                 </div>
                 ): (<></>)}
